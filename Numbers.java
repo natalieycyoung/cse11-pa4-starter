cf4715b77904ef33d0cae221a13968a280286011
natalieycyoung <44858512+natalieycyoung@users.noreply.github.com>
2021-11-27 19:26:42 -0800

    Passed all tests but 1

--- Numbers.java
+++ Numbers.java
@@ -6,6 +6,7 @@
  */
 
 import tester.*;
+import java.io.File;
 
 interface Number
 {
@@ -53,13 +54,13 @@ class WholeNumber implements Number
 	 */
 	public Number add(Number other)
 	{
-		int sumNum = 0;
+		int sumNum = this.numerator();
 		int sumDenom = 1;
 		Number sum;
 
 		if (other.denominator() == 1)
 		{
-			sumNum = this.numerator() + other.numerator();
+			sumNum += other.numerator();
 
 			sum = new WholeNumber(sumNum);
 		}
@@ -113,6 +114,7 @@ class WholeNumber implements Number
 	 */
 	public double toDouble()
 	{
+	//	double n = this.n;
 		return (double)n;
 	}
 }
@@ -204,9 +206,51 @@ class Fraction implements Number
 	 */
 	public double toDouble()
 	{
-		return (double)(n/d);
+		return (double)n/(double)d;
 	}
 }
 
 class ExamplesNumbers
-{}
+{
+	Number n1 = new WholeNumber(5);
+	Number n2 = new WholeNumber(7);
+	Number n3 = new Fraction(7, 2);
+	Number n4 = new Fraction(1, 2);
+	
+	void testAdd(Tester t)
+	{
+		t.checkExpect(this.n1.add(this.n2).toDouble(), 12.0);
+		t.checkExpect(this.n1.add(this.n3).toDouble(), 5 + 7.0/2.0);
+		t.checkExpect(this.n3.add(this.n3).toDouble(), 7.0);
+	}
+	
+	void testMultiply(Tester t)
+	{
+		t.checkExpect(this.n1.multiply(this.n4).toDouble(), 2.5);
+		t.checkExpect(this.n3.multiply(this.n4).toDouble(), 7.0/4.0);
+	}
+	
+	void testNumDem(Tester t)
+	{
+		t.checkExpect(this.n3.numerator(), 7);
+		t.checkExpect(this.n1.numerator(), 5);
+		t.checkExpect(this.n4.denominator(), 2);
+		t.checkExpect(this.n2.denominator(), 1);
+	}
+	
+	void testToString(Tester t)
+	{
+		t.checkExpect(this.n4.toText(), "1/2");
+		t.checkExpect(this.n3.toText(), "7/2");
+		t.checkExpect(this.n2.toText(), "7");
+	}
+
+
+	// EXPLORATION
+/*	
+	public static void main(String args[])
+	{
+		
+	}
+*/
+}
